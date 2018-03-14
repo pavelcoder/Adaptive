28897f3864a9d74d0bb330f691ae78a8a92160dd
Pavel <pavelcoder@gmail.com>
2018-03-14 18:32:25 +0500

    init

---
+++ NetworkDownloader.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   NetworkDownloader.cpp
+ * Author: pavel
+ * 
+ * Created on 14 марта 2018 г., 18:18
+ */
+
+#include "NetworkDownloader.h"
+
+NetworkDownloader::NetworkDownloader() {
+}
+
+NetworkDownloader::NetworkDownloader(const NetworkDownloader& orig) {
+}
+
+NetworkDownloader::~NetworkDownloader() {
+}
+
---
+++ build/Debug/GNU-Linux/NetworkDownloader.o.d
@@ -0,0 +1,4 @@
+build/Debug/GNU-Linux/NetworkDownloader.o: NetworkDownloader.cpp \
+ NetworkDownloader.h
+
+NetworkDownloader.h:
